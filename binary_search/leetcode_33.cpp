d81ad503ac09129cf25f45005bb957144214eea4
chiranth070 <1ms22ci019@msrit.edu>
2024-04-21 21:56:21 +0530

    2nd commit

--- binary_search/leetcode_33.cpp
+++ binary_search/leetcode_33.cpp
@@ -14,18 +14,20 @@ public:
             //check if array is left sorted
             if(nums[mid]>=nums[low])
             {
+                //check if target is present in left sorted part
                 if(nums[mid]>=target && nums[low]<=target)
-                high=mid-1;
+                high=mid-1; //eleminate right part
                 else
-                low=mid+1;
+                low=mid+1;  //eleminate left part
             }
             //check if array is rigt sorted
             else
             {                
+                //check if target is present in right sorted part
                 if(nums[mid]<=target && nums[high]>=target)
-                low=mid+1;
+                low=mid+1;  //eleminate left part
                 else
-                high=mid-1;
+                high=mid-1; //eleminate right part
             }
         }
         return -1;
