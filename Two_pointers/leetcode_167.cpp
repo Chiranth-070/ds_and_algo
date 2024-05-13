222f67bf376ccc67c5d3547f240e253adc0233e1
chiranth070 <152704439+Chiranth-070@users.noreply.github.com>
2024-05-13 22:26:09 +0530

    ***

---
+++ Two_pointers/leetcode_167.cpp
@@ -0,0 +1,19 @@
+// 167. Two Sum II - Input Array Is Sorted
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0,j=numbers.size()-1;
+        while(i < j)
+        {
+            int sum=numbers[i]+numbers[j];
+            if(sum == target)
+            return {++i,++j};
+            else if(sum < target)
+            i++;
+            else
+            j--;
+        }
+        return {};
+    }
+};
---
+++ Two_pointers/leetcode_392.cpp
@@ -0,0 +1,20 @@
+// 392. Is Subsequence
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sp = 0;
+        int tp = 0;
+        while(sp<s.length() && tp<t.length())
+        {
+            if(s[sp]==t[tp])
+            {
+                sp++;
+                tp++;
+            }
+            else
+            tp++;
+        }
+        return sp == s.length();
+    }
+};
